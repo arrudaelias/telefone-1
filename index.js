59c8f4e88466cbea60c6914875e82a67189f5520
Elias Arruda <>
2022-09-09 06:41:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+window.function = function (cpf) {
+cpf = cpf.value ?? "";
+cpf=cpf.replace(/\D/g,"")
+cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")  
+cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2") 
+cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+  return cpf.replace=(cpf);
+}
